42499bff49a0e6c7598529eabb0d3fcf745f1f0d
Jean Cesar <neo.jean@gmail.com>
2018-05-04 18:29:44 -0300

    Validacao de campos e contador

--- public/js/main.js
+++ public/js/main.js
@@ -6,6 +6,7 @@ $(function(){
   atualizaTamanhoFrase();
   inicializaContadores();
   inicializaCronometro();
+  inicializaMarcadores();
   $('#botao-reiniciar').click(reiniciaJogo);
 });
 
@@ -37,6 +38,7 @@ function inicializaCronometro() {
         campo.attr("disabled", true);
         clearInterval(cronometroID);
         $("#botao-reiniciar").attr("disabled", false);
+        campo.toggleClass("campo-desativado");
       }
     }, 1000);
   });
@@ -44,10 +46,28 @@ function inicializaCronometro() {
 
 function reiniciaJogo(){
   campo.attr("disabled", false);
-
   campo.val("");
   $('.contador-palavras').text("0");
   $('.contador-caracteres').text("0");
   $('#tempo-digitacao').text(tempoInicial);
   inicializaCronometro();
+  campo.toggleClass("campo-desativado");
+  
+  campo.removeClass("borda-vermelha");
+  campo.removeClass("borda-verde");
 };
+
+function inicializaMarcadores() {
+  var frase = $('.frase').text();
+  campo.on('input', function(){
+    var digitado = campo.val();
+    
+    if( frase.startsWith(digitado)) {
+      campo.addClass("borda-verde");
+      campo.removeClass('borda-vermelha');
+    } else {
+      campo.addClass("borda-vermelha");
+      campo.removeClass('borda-verde');
+    }
+  });
+}
